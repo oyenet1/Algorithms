3e4be0b9847def0212118766040e1f16339cd704
oyenet1 <bowofadeoyerinde@gmail.com>
2023-01-11 03:39:49 +0100

    valid parentheses added

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -7,18 +7,31 @@ class TwoSum {
 
   // method one by creating a distinct array
   methodOne() {
-    let hashMap = {};
+    let hMap = {};
+    let ntF;
     for (let i = 0; i < this.nums.length; i++) {
-      let nTF = this.target - this.nums[i];
-      if (nTF in hashMap) {
-        return [hashMap[nTF], i];
+      ntF = this.target - this.nums[i];
+      if (hMap[ntF] && hMap[ntF] !== i) {
+        return [hMap[ntF], i];
       }
-      hashMap[this.nums[i]] = i;
+      hMap[this.nums[i]] = i;
+    }
+    return -1;
+  }
+  methodTwo() {
+    // let pNum = this.target - this.nums[0];
+    for (let i = 1; i < this.nums.length; i++) {
+      for (let j = i + 1; j < this.nums.length; j++) {
+        if ((this.nums[i] + this.nums[j]) === this.target) {
+          return [i, j];
+        }
+      }
     }
     return -1;
   }
 }
-var numbers = [6, 1, 3, 4, 2, 8];
+var numbers = [6, 1, 3, 4, 2, 7];
 
-let first = new TwoSum(numbers, 6);
+let first = new TwoSum(numbers, 5);
 console.log(first.methodOne());
+console.log(first.methodTwo());
